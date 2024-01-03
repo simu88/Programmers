d96ce14de168cd9e8b327dc1f1733516894d3228
simu88 <105586812+simu88@users.noreply.github.com>
2024-01-03 19:13:09 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.13 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -2,24 +2,20 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
-        answer[0]=-1;
-
-        for(int i=1;i<s.length();i++){
-            char c1=s.charAt(i);
-            int gap=-1;
-            for(int j=0;j<i;j++){
-                char c2=s.charAt(j);
-                if(c1==c2){
-                    gap=i-j;
-                }
-                
+        
+        Map<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            
+            if(map.containsKey(c)){
+                answer[i]=i-map.get(c);
+            }else{
+                answer[i]=-1;
             }
+               map.put(c,i);
+              
             
-                answer[i]=gap;
-            
-        
         }
-        
         
         return answer;
     }
